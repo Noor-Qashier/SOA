91415f6a17a407d811db853d19df5107efde200a
Noor-Qashier <67406655+Noor-Qashier@users.noreply.github.com>
2020-12-22 17:05:29 +0800

    correct connection

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-$mysqli = new mysqli ('localhost', 'root','', 'soa_pvcs');
-
+$mysqli = new mysqli ('localhost', 'proveri2_admin','PVCS-001', 'proveri2_soa');
+//$mysqli = new mysqli ('localhost', 'root','', 'soa_pvcs');
 ?>
