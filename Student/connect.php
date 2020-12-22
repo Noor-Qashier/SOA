78f7682bdfd1924b5d857771e3b3f4d3b16381fb
Noor-Qashier <67406655+Noor-Qashier@users.noreply.github.com>
2020-12-22 16:37:13 +0800

    overall update

--- Student/connect.php
+++ Student/connect.php
@@ -1,4 +1,5 @@
 <?php
 
-	$mysqli = new PDO("mysql:host=localhost;dbname=soa_pvcs","root","");
+	//$mysqli = new PDO("mysql:host=localhost;dbname=soa_pvcs","root","");
+	$mysqli = new PDO("mysql:host=localhost;dbname=proveri2_soa","proveri2_admin","PVCS-001");
 ?>
