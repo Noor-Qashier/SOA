b3d113bcb0967dd96fecd4c68bf363734c6dec7e
Noor-Qashier <67406655+Noor-Qashier@users.noreply.github.com>
2021-01-05 15:25:28 +0800

    data connect

--- Student/connect.php
+++ Student/connect.php
@@ -1,4 +1,4 @@
 <?php
-	//$mysqli = new PDO("mysql:host=localhost;dbname=proveri2_soa","proveri2_admin","PVCS-001");
-	$mysqli = new PDO("mysql:host=localhost;dbname=soa_pvcs","root","");
+	$mysqli = new PDO("mysql:host=localhost;dbname=proveri2_soa","proveri2_admin","PVCS-001");
+	//$mysqli = new PDO("mysql:host=localhost;dbname=soa_pvcs","root","");
 ?>
